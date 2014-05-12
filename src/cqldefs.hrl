910716213db20566bb4c4ecba9c72ef537d8cf74
Alex Petrov <alexp@coffeenco.de>
2014-05-12 23:25:48 +0100

    Use atoms instead of integers for constants in protocol definitions

--- src/cqldefs.hrl
+++ src/cqldefs.hrl
@@ -20,21 +20,52 @@
 -define(OPCODE_AUTH_RESPONSE,  13).
 -define(OPCODE_AUTH_SUCCESS,   14).
 
--define(FRAME_TYPE_REQUEST,   2#00000011).
--define(FRAME_TYPE_RESPONSE,  16#83).
+-define(FRAME_TYPE_REQUEST,    2#0).
+-define(FRAME_TYPE_RESPONSE,   2#1).
 
--define(frame_part, 8/unsigned-integer).
+-define(FRAME_VERSION,         2#0000010).
+
+-define(frame_part_type, 1/unsigned-integer).
+-define(frame_part_version, 7/unsigned-integer).
+-define(frame_part_flags, 8/unsigned-integer).
+-define(frame_part_opcode, 8/unsigned-integer).
 -define(frame_part_stream, 8/signed-integer).
+
 -define(short, 1/big-unsigned-unit:16).
 -define(int,   1/big-signed-unit:32).
 -define(long,  1/big-signed-unit:64).
 
 
 -record(frame, {
-          version  = ?FRAME_TYPE_REQUEST,
+          type     = request,
+          version  = ?FRAME_VERSION,
           flags    = 0,
           stream   = 1,
           opcode,
           length,
           body =   <<>>
          }).
+
+
+type_to_atom(?FRAME_TYPE_REQUEST)  -> request;
+type_to_atom(?FRAME_TYPE_RESPONSE) -> response.
+
+type_from_atom(request)            -> ?FRAME_TYPE_REQUEST;
+type_from_atom(response)           -> ?FRAME_TYPE_RESPONSE.
+
+
+opcode_to_atom(?OPCODE_ERROR)             -> error;
+opcode_to_atom(?OPCODE_STARTUP)           -> startup;
+opcode_to_atom(?OPCODE_READY)             -> ready;
+opcode_to_atom(?OPCODE_AUTHENTICATE)      -> authenticate;
+opcode_to_atom(?OPCODE_OPTIONS)           -> options;
+opcode_to_atom(?OPCODE_SUPPORTED)         -> supported;
+opcode_to_atom(?OPCODE_QUERY)             -> query;
+opcode_to_atom(?OPCODE_PREPARE)           -> prepare;
+opcode_to_atom(?OPCODE_EXECUTE)           -> execute;
+opcode_to_atom(?OPCODE_REGISTER)          -> register;
+opcode_to_atom(?OPCODE_EVENT)             -> event;
+opcode_to_atom(?OPCODE_BATCH)             -> batch;
+opcode_to_atom(?OPCODE_AUTH_CHALLENGE)    -> auth_challenge;
+opcode_to_atom(?OPCODE_AUTH_RESPONSE)     -> auth_response;
+opcode_to_atom(?OPCODE_AUTH_SUCCESS)      -> auth_success.
--- src/polyxena.erl
+++ src/polyxena.erl
@@ -20,6 +20,9 @@ cql_string(Str) when is_binary(Str) ->
     Size = size(Str),
     [cql_short(Size), Str].
 
+decode_cql_string(<<Length:?short, Str:Length/binary, _/binary>>) ->
+    Str.
+
 cql_long_string(Str) when is_binary(Str) ->
     Size = size(Str),
     [cql_int(Size), Str].
@@ -41,8 +44,6 @@ cql_map(Map) when is_list(Map) ->
 
 %% API
 
-
-
 init_client(Host, Port) ->
     Opts = [
             {active, false},
@@ -58,33 +59,64 @@ init_client(Host, Port) ->
             io:format("Can't connect to remote: '~w'~n", [Why])
     end.
 
-frame_to_binary(#frame{version = Version,
+frame_to_binary(#frame{type = TypeAtom,
+                       version = Version,
                        flags   = Flags,
                        stream  = Stream,
                        opcode  = Opcode,
                        body    = Body}) ->
+    Type   = type_from_atom(TypeAtom),
     Length = iolist_size(Body),
-    [<<
-       Version:?frame_part,
-       Flags:?frame_part,
+    [<<Type:?frame_part_type,
+       Version:?frame_part_version,
+       Flags:?frame_part_flags,
        Stream:?frame_part_stream,
-       Opcode:?frame_part,
-       Length:32/big-unsigned-integer
-     >>,
+       Opcode:?frame_part_opcode,
+       Length:32/big-unsigned-integer>>,
      Body].
 
-tryout() ->
-    Socket = polyxena:init_client("192.168.60.15", 9042),
+decode_body(Opcode, Body) ->
+    case opcode_to_atom(Opcode) of
+        error ->
+            <<ErrCode:?int, ErrMsg/binary>> = Body,
+            {ErrCode, decode_cql_string(ErrMsg)};
+        ready ->
+            ok
+    end.
+
+
+decode_response(<<Type:?frame_part_type,
+                  Version:?frame_part_version,
+                  Flags:?frame_part_flags,
+                  Stream:?frame_part_stream,
+                  Opcode:?frame_part_opcode,
+                  Length:32/big-unsigned-integer,
+                  Body:Length/binary-unit:8,
+                  _/binary>>) ->
+    %% io:format("Type: ~p Version ~p Flags ~p Stream: ~p  Opcode: ~p Length ~p Body ~p ~n", [Type, Version, Flags, Stream, Opcode, Length, Body]).
+    io:format("Type: ~p Version: ~p Flags ~p Stream: ~p  Opcode: ~p ~n Length ~p ~n Body ~p ~n",
+              [type_to_atom(Type),
+               Version, Flags, Stream,
+               opcode_to_atom(Opcode),
+               Length,
+               decode_body(Opcode, Body)
+               ]).
+
+establish_connection(Host, Port) ->
+    Socket = polyxena:init_client(Host, Port),
     StartupFrame = #frame{
-                      version = ?FRAME_TYPE_REQUEST,
                       opcode = ?OPCODE_STARTUP,
                       body = cql_map([{<<"CQL_VERSION">>, ?CQL_VERSION}])
                      },
     EncodedFrame = frame_to_binary(StartupFrame),
     io:format("Frame ~p ~n", [EncodedFrame]),
     gen_tcp:send(Socket, EncodedFrame),
-    io:format("Waiting for response ~n"),
     {ok, Result} = gen_tcp:recv(Socket, 0),
-    io:format("Got Response ~p", [Result]).
+    io:format("Got Response ~p ~n", [Result]),
+    decode_response(Result).
 
-    %% End of Module.
+%% End of Module.
+
+tryout() ->
+    establish_connection("192.168.60.15", 9042).
+%% polyxena:tryout().
