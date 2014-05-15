9e2cfe784d2582faeac4bdfbe2f2742597377210
Alex Petrov <alexp@coffeenco.de>
2014-05-16 00:17:25 +0100

    Start working at result decoding

--- src/cqldefs.hrl
+++ src/cqldefs.hrl
@@ -20,15 +20,15 @@
 -define(OPCODE_AUTH_RESPONSE,  13).
 -define(OPCODE_AUTH_SUCCESS,   14).
 
--define(CONSISTENCY_ANY,           10#0).
--define(CONSISTENCY_ONE,           10#1).
--define(CONSISTENCY_TWO,           10#2).
--define(CONSISTENCY_THREE,         10#3).
--define(CONSISTENCY_QUORUM,        10#4).
--define(CONSISTENCY_ALL,           10#5).
--define(CONSISTENCY_LOCAL_QUORUM,  10#6).
--define(CONSISTENCY_EACH_QUORUM,   10#7).
--define(CONSISTENCY_LOCAL_ONE,     10#10).
+-define(CONSISTENCY_ANY,           0).
+-define(CONSISTENCY_ONE,           1).
+-define(CONSISTENCY_TWO,           2).
+-define(CONSISTENCY_THREE,         3).
+-define(CONSISTENCY_QUORUM,        4).
+-define(CONSISTENCY_ALL,           5).
+-define(CONSISTENCY_LOCAL_QUORUM,  6).
+-define(CONSISTENCY_EACH_QUORUM,   7).
+-define(CONSISTENCY_LOCAL_ONE,     10).
 
 -define(RESULT_KIND_VOID,               10#1).
 -define(RESULT_KIND_ROWS,               10#2).
@@ -39,13 +39,14 @@
 -define(FRAME_TYPE_REQUEST,    2#0).
 -define(FRAME_TYPE_RESPONSE,   2#1).
 
--define(FRAME_VERSION,         2#0000010).
+-define(FRAME_VERSION,         2#0000001).
 
 -define(frame_part_type, 1/unsigned-integer).
 -define(frame_part_version, 7/unsigned-integer).
+-define(frame_part_stream, 8/signed-integer).
 -define(frame_part_flags, 8/unsigned-integer).
 -define(frame_part_opcode, 8/unsigned-integer).
--define(frame_part_stream, 8/signed-integer).
+
 
 -define(short, 1/big-unsigned-unit:16).
 -define(int,   1/big-signed-unit:32).
--- src/polyxena.erl
+++ src/polyxena.erl
@@ -19,8 +19,7 @@ cql_encode(short, Short)  when is_integer(Short)   -> <<Short:?short>>;
 cql_encode(long, Long)    when is_integer(Long)    -> <<Long:?long>>;
 
 cql_encode(string, Str) when is_binary(Str) ->
-    Size = size(Str),
-    [cql_encode(short, Size), Str].
+    [cql_encode(short, size(Str)), Str].
 
 decode_cql_string(<<Length:?short, Str:Length/binary-unit:8, _/binary>>) ->
     binary_to_list(Str).
@@ -83,14 +82,46 @@ decode_body(Opcode, Body) ->
             <<ErrCode:?int, ErrMsg/binary>> = Body,
             {error, ErrCode, decode_cql_string(ErrMsg)};
         ?OPCODE_READY ->
-            {ok};
+            {ready};
         ?OPCODE_RESULT ->
             <<ResultKind:?int, Rest/binary>> = Body,
             {ok, decode_result_kind(ResultKind, Rest)}
     end.
 
 decode_result_kind(?RESULT_KIND_SET_KEYSPACE, Body) ->
-    decode_cql_string(Body).
+    {ok,
+     decode_cql_string(Body)};
+
+decode_result_kind(?RESULT_KIND_SCHEMA_CHANGE,
+                   <<Length1:?short,
+                     Str1:Length1/binary-unit:8,
+                     Length2:?short,
+                     Str2:Length2/binary-unit:8,
+                     Length3:?short,
+                     Str3:Length3/binary-unit:8,
+                     _/binary>>) ->
+    {schema_change,
+     binary_to_list(Str1),
+     binary_to_list(Str2),
+     binary_to_list(Str3)};
+
+decode_result_kind(?RESULT_KIND_VOID, _) ->
+    {void};
+
+%% QUESTION: ?? Can you conj to the tuple??
+decode_result_kind(?RESULT_KIND_ROWS, <<Flags:?int,
+                                        ColumnsCount:?int,
+                                        Rest/binary>>) ->
+    {Flags, ColumnsCount, Rest}.
+
+decode_rows_flags(Flags) ->
+    lists:foldl(fun({Flag, Mask}, Acc) ->
+                        io:format("a: ~s\n",[Acc]),
+                        if Flags band Mask == Mask -> [Flag | Acc];
+                           true -> Acc
+                        end
+                end, [], [{global_tables_spec, 1}]).
+
 
 
 receive_frame(Socket) ->
@@ -117,6 +148,7 @@ receive_frame(Socket) ->
 
 send_frame(Socket, Frame) ->
     EncodedFrame = frame_to_binary(Frame),
+    %% io:format("SEND: ~p\n",[iolist_to_binary(EncodedFrame)]),
     gen_tcp:send(Socket, EncodedFrame).
 
 startup_frame() ->
@@ -129,21 +161,40 @@ query_frame(Query, Consistency) when is_list(Query) ->
 
 query_frame(Query, Consistency) ->
     #frame{opcode = ?OPCODE_QUERY,
-           body = [cql_long_string(Query), cql_encode(int, Consistency)]}.
+           body = [cql_long_string(Query),
+                   cql_encode(short, Consistency)
+                   ]}.
 
 establish_connection(Host, Port) ->
     Socket = polyxena:init_client(Host, Port),
     send_frame(Socket, startup_frame()),
     case receive_frame(Socket) of
-        {ok} ->
+        {ready} ->
             Socket
     end.
 
 %% End of Module.
 
 tryout() ->
-    Socket = establish_connection("192.168.60.15", 9042),
-    send_frame(Socket, query_frame("use instana2;", ?CONSISTENCY_ONE)),
-    io:format("Response: ~w  ~n", receive_frame(Socket)).
+    Connection = establish_connection("192.168.60.15", 9042),
+
+    execute_cql(Connection, "DROP KEYSPACE \"cassaforte_keyspace\";"),
+    execute_cql(Connection, "CREATE KEYSPACE \"cassaforte_keyspace\" WITH replication = {'class' : 'SimpleStrategy', 'replication_factor' : 1};"),
+    execute_cql(Connection, "USE \"cassaforte_keyspace\";"),
+    execute_cql(Connection, "CREATE TABLE \"users\" (age int, name varchar, PRIMARY KEY (name));"),
+    execute_cql(Connection, "INSERT INTO \"users\" (name, age) VALUES ('Alex', 19);"),
+    execute_cql(Connection, "SELECT * from users").
+
+    %% execute_cql(Connection, "use my_keyspace;").
+    %% execute_cql(Connection, "use instana2;").
+    %% execute_cql(Connection, "select * from rollups limit 1;").
+
+    %% send_frame(Socket, query_frame("use instana2;", ?CONSISTENCY_ONE)),
+
 
     %% polyxena:tryout().
+
+
+execute_cql(Connection, Query) ->
+    send_frame(Connection, query_frame(Query, ?CONSISTENCY_ONE)),
+    receive_frame(Connection).
